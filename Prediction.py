47f3ba6d047b979743b5b2560aa5c39ff90b3df0
stajyer <sozencagla@outlook.com>
2018-09-06 09:05:16 +0300

    Initial Commit

---
+++ Prediction.py
@@ -0,0 +1,118 @@
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_absolute_error
+import os
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.linear_model import Ridge
+from xgboost import XGBRegressor
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import Perceptron
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+from sklearn.svm import SVC
+
+
+df = pd.read_csv('/home/stajyer/Desktop/RaisePrediction/dataAddedLMHFinal.csv')
+df.head()
+df.isnull().sum()
+
+df['TotalQ'] = df['LevelOfHike']
+df['TotalQ'].loc[df.TotalQ == 'L'] = 0.0
+df['TotalQ'].loc[df.TotalQ == 'M'] = 1.0
+df['TotalQ'].loc[df.TotalQ == 'H'] = 2.0
+
+X = df.ix[:,[0,1,2,3,4,6,7,9,10,12,13,14,15,16,18,25,30,33,34,35]]
+y = np.array(df['TotalQ'])
+
+
+X = pd.get_dummies(X)
+
+
+X.shape
+
+
+df['Totall'] = df['LevelOfHike']
+df['Totall'].loc[df.Totall == 'L'] = 0.0
+df['Totall'].loc[df.Totall == 'M'] = 1.0
+df['Totall'].loc[df.Totall == 'H'] = 2.0
+
+#continuous_subset = df.ix[:,[0,1,2,3,4,6,7,10,12,13,14,15,20,22,25,29,30,31,32,33,34,35]]
+#one_hot_encoded_training_predictors = pd.get_dummies(continuous_subset)
+#continuous_subset = one_hot_encoded_training_predictors.ix[:,[0,2,3,4,6,7,10,12,13,14,15,16,18,20,22,25,29,30,31,32,33,34,35]]
+#continuous_subset = pd.get_dummies(continuous_subset)
+X = df.ix[:,[0,1,2,3,4,6,7,10,12,13,14,15,16,18,25,28,33,34,35]]
+y = np.array(df['Totall'])
+
+
+X = pd.get_dummies(X)
+
+
+X.shape
+
+names = ['DecisionTreeRegressor', 'LinearRegression', 'Ridge', 'Lasso','XGBRegressor']
+
+clf_list = [DecisionTreeRegressor(),
+            LinearRegression(),
+            Ridge(),
+            Lasso(),XGBRegressor()]
+
+for name, clf in zip(names, clf_list):
+    print(name, end=': ')
+    print(cross_val_score(clf, X, y, cv=5).mean())
+
+tree = DecisionTreeRegressor()
+tree.fit(X, y)
+
+importances = tree.feature_importances_
+indices = np.argsort(importances)[::-1]
+for f in range(X.shape[1]):
+    print("%d. Feature %s (%f)" % (f + 1, X.columns.values[indices[f]], importances[indices[f]]))
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(
+         X, y, test_size=0.35, random_state=0)
+
+
+sc = StandardScaler()
+
+sc.fit(X_train)
+
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+
+ppn = Perceptron(n_iter=40, eta0=0.1, random_state=0)
+ppn.fit(X_train_std, y_train)
+y_pred = ppn.predict(X_test_std)
+#print('Misclassified samples: %d' % (y_test != y_pred).sum()
+
+
+print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+
+svm = SVC(kernel='linear', C=2.0, random_state=0)
+svm.fit(X_train_std, y_train)
+
+y_pred = svm.predict(X_test_std)
+print('Misclassified samples: %d' % (y_test != y_pred).sum())
+print("Mean Absolute Error : " + str(mean_absolute_error(y_pred, y_test)))
+
+
+
+print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
+
+my_model = XGBRegressor()
+my_model.fit(X_train_std, y_train, verbose=False)
+y_pred = my_model.predict(X_test_std)
+print('Misclassified samples for XGBoost: %d' % (y_test != y_pred).sum())
+print("Mean Absolute Error : " + str(mean_absolute_error(y_pred, y_test)))
+
+print("XGBoost was no better than SVC")
---
+++ Visualization.py
@@ -0,0 +1,125 @@
+import pandas as pd
+import matplotlib as plt
+import seaborn as sns
+import matplotlib.pyplot as mp
+
+dataset = pd.read_csv("/home/stajyer/Desktop/RaisePrediction/HREmployeeDataset.csv")
+sns.set_palette("Reds")
+target = dataset.PercentSalaryHike
+
+print("Dataset Size:",dataset.shape, end = "\n\n")
+
+print("Column values are: ",list(dataset), end = "\n\n")
+
+print("Showing some target values", target.head(),sep = "\n\n")
+
+#PERCENT SALARY VS ATTRIBUTES
+
+#First Attribute to evaluate with respect to PercentSalaryHike is Business Travel
+
+print( "---First Attribute to evaluate is Business Travel with STRING values ---" ,dataset.BusinessTravel.head() ,sep = "\n")
+
+first= sns.boxplot(x="BusinessTravel", y="PercentSalaryHike", data=dataset)
+figure = first.get_figure()
+#figure.savefig('BusinessTrav_plot.png', dpi=400)
+
+#Second Attribute to evaluate with respect to PercentSalaryHike is Distance From Home
+
+
+print( "---Second Attribute to evaluate is  Distance From Home with INT values ---" ,dataset.DistanceFromHome.head() ,sep = "\n")
+
+mp.figure()
+
+#second= dataset.plot.hexbin(x='DistanceFromHome', y='PercentSalaryHike',gridsize=15,cmap=plt.cm.Reds)
+second = sns.lineplot(x='DistanceFromHome', y='PercentSalaryHike',data=dataset)
+figure = second.get_figure()
+figure.savefig('Distance2_plot.png', dpi=400)
+
+#Third Attribute to evaluate with respect to PercentSalaryHike is Employee Count
+
+
+print( "---Third Attribute to evaluate is  Employee Count with INT values ---" ,dataset.EmployeeCount.head() ,sep = "\n")
+
+third = dataset.plot.line(x='EmployeeCount', y='PercentSalaryHike')
+
+figure = third.get_figure()
+#figure.savefig('EmployeeCount_plot.png', dpi=400)
+
+#Fourth Attribute to evaluate with respect to PercentSalaryHike is Gender
+
+print( "---Fourth Attribute to evaluate is Gender with STRING values ---" ,dataset. Gender.head() ,sep = "\n")
+
+mp.figure()
+fourth = sns.barplot(x="Gender", y="PercentSalaryHike", data=dataset)
+figure = fourth.get_figure()
+#figure.savefig('Gender_plot.png', dpi=400)
+#Fifth Attribute to evaluate with respect to PercentSalaryHike is Job Level
+
+print( "---Fifth Attribute to evaluate is Job Level with INT values ---" ,dataset. JobLevel.head() ,sep = "\n")
+
+mp.figure()
+fifth = sns.lineplot(x = "JobLevel", y = "PercentSalaryHike",data=dataset)
+figure = fifth.get_figure()
+#figure.savefig('JobLevel_plot.png', dpi=400)
+
+#Sixth Attribute to evaluate with respect to PercentSalaryHike is Marital Status
+
+print( "---Sixth Attribute to evaluate is  Marital Status with STRING values ---" ,dataset. MaritalStatus.head() ,sep = "\n")
+
+mp.figure()
+
+sixth = sns.barplot(x="MaritalStatus", y="PercentSalaryHike", data=dataset)
+figure = sixth.get_figure()
+#figure.savefig('Marital_plot.png', dpi=400)
+
+#Seventh Attribute to evaluate with respect to PercentSalaryHike is NumCompaniesWorked
+
+print( "---Seventh Attribute to evaluate is NumCompaniesWorked with INT values ---" ,dataset.NumCompaniesWorked.head() ,sep = "\n")
+
+mp.figure()
+
+seventh = sns.lineplot(x = "NumCompaniesWorked", y = "PercentSalaryHike",data=dataset)
+figure = seventh.get_figure()
+#figure.savefig('CompaniesWorked_plot.png', dpi=400)
+
+#Eight Attribute to evaluate with respect to PercentSalaryHike is StandardHours
+
+
+print( "---Eight Attribute to evaluate is  Standard Hours with STRING values ---" ,dataset.StandardHours.head() ,sep = "\n")
+
+eight = dataset.plot.line(x='StandardHours', y='PercentSalaryHike')
+figure = eight.get_figure()
+#figure.savefig('StandardHours_plot.png', dpi=400)
+
+#Ninth Attribute to evaluate with respect to PercentSalaryHike is TrainingTimesLastYear
+
+print( "---Ninth Attribute to evaluate is  TrainingTimesLastYear with INT values ---" ,dataset.TrainingTimesLastYear.head() ,sep = "\n")
+
+
+mp.figure()
+
+ninth = sns.lineplot(x='TrainingTimesLastYear', y='PercentSalaryHike',data=dataset)
+figure = ninth.get_figure()
+#figure.savefig('TrainingTimes.png', dpi=400)
+
+#Tenth Attribute to evaluate with respect to PercentSalaryHike is YearsInCurrentRole
+
+print( "---Tenth Attribute to evaluate is YearsInCurrentRole with INT values ---" ,dataset.YearsInCurrentRole.head() ,sep = "\n")
+
+mp.figure()
+
+tenth = sns.lineplot(x='YearsInCurrentRole', y='PercentSalaryHike',data=dataset)
+figure = tenth.get_figure()
+#figure.savefig('YearsInCurrentRole_plot.png', dpi=400)
+
+#Eleventh Attribute to evaluate with respect to PercentSalaryHike is YearsWithCurrManager
+
+print( "---Eleventh Attribute to evaluate is YearsWithCurrManager with INT values ---" ,dataset.YearsWithCurrManager.head() ,sep = "\n")
+
+mp.figure()
+
+eleventh = sns.lineplot(x='YearsWithCurrManager', y='PercentSalaryHike',data=dataset)
+figure = eleventh.get_figure()
+#figure.savefig('YearsWithManager_plot.png', dpi=400)
+
+mp.show()
